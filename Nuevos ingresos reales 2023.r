813b118f9dd54ff7f657449c44e9e5352402e614
Rodrigo Franco <rodrigofrancofuentes@gmail.com>
2023-09-12 23:01:11 +0200

    código funciona para ingresos mensuales por nivel educativo

---
+++ Nuevos ingresos reales 2023.r
@@ -0,0 +1,8 @@
+library(foreign) #para leer .dbf
+library(stringr) #para str_c
+library(dplyr) #para left_join
+library(reldist) #wtd.quantile
+library(ggplot2) #Gráficas
+library(readODS) #Para leer el archivo del deflactor
+library(tidyr) # Para gather
+
